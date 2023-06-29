3b08f2a95f5cbf363e11ec57e91ad75e897bdeb2
saul.sanchez <saul.sanchez@globant.com>
2023-06-29 16:21:28 -0600

    Add entity for saving information about API

---
+++ src/main/java/com/challenge/marvel/rest/model/RegisterLog.java
@@ -0,0 +1,49 @@
+package com.challenge.marvel.rest.model;
+
+
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Builder
+@Table(name = "RegisterLog")
+@AllArgsConstructor
+public class RegisterLog {
+
+    @Id
+    @GeneratedValue()
+    private Long id;
+
+    @Column
+    private String api;
+
+    @Column
+    private Integer status;
+
+    @Column
+    private LocalDateTime invocationDateTime;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        RegisterLog that = (RegisterLog) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return getClass().hashCode();
+    }
+}
