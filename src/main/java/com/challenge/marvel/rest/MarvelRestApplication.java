505be9e492ae36b68bf7e6d9a137b495fcf831fc
saul.sanchez <saul.sanchez@globant.com>
2023-06-28 20:07:58 -0600

    Initial project

---
+++ src/main/java/com/challenge/marvel/rest/MarvelRestApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.marvel.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelRestApplication.class, args);
+	}
+
+}
