4770703169b84a67f07b200fad8adf8479c79552
saul.sanchez <saul.sanchez@globant.com>
2023-06-29 17:15:10 -0600

    Add version of APIs

--- src/main/java/com/challenge/marvel/rest/controller/MarvelRestController.java
+++ src/main/java/com/challenge/marvel/rest/controller/MarvelRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/marvel")
+@RequestMapping("/api/v1/marvel")
 @RequiredArgsConstructor
 public class MarvelRestController {
 
--- src/main/java/com/challenge/marvel/rest/controller/RegisterLogController.java
+++ src/main/java/com/challenge/marvel/rest/controller/RegisterLogController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/log")
+@RequestMapping("/api/v1/log")
 @RequiredArgsConstructor
 public class RegisterLogController {
 
